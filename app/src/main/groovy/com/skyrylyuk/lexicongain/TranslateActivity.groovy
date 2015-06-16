10b2c9e5658916b748ae4af6fcbbe8f3e1ea06cc
skyrylyuk <sergii@airbeem.com>
2015-06-16 14:29:54 +0300

    Implement translate and show result

---
+++ app/src/main/groovy/com/skyrylyuk/lexicongain/TranslateActivity.groovy
@@ -0,0 +1,97 @@
+package com.skyrylyuk.lexicongain
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.arasthel.swissknife.SwissKnife
+import com.arasthel.swissknife.annotations.InjectView
+import com.arasthel.swissknife.annotations.OnBackground
+import com.arasthel.swissknife.annotations.OnUIThread
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
+import org.json.JSONArray
+import org.json.JSONObject
+
+/**
+ * Airbeem project
+ * Created by skyrylyuk on 6/16/15.
+ */
+class TranslateActivity extends Activity {
+    public static final String API_KEY = 'trnsl.1.1.20150616T094415Z.ab702c0fb8064c33.a4bb58f089d2c9167ce05c0078ffd982a25f982e'
+
+    OkHttpClient client = new OkHttpClient();
+
+    @InjectView(R.id.txvOriginal)
+    TextView txvOriginal
+
+    @InjectView(R.id.prbTranslate)
+    ProgressBar prbTranslate
+
+    @InjectView(R.id.txvTranslate)
+    TextView txvTranslate
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView R.layout.activity_translate
+
+        SwissKnife.inject this
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (action == Intent.ACTION_SEND) {
+            String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            txvOriginal.text = sharedText
+
+            translateOnBackground sharedText
+        }
+    }
+
+    @OnBackground()
+    public void translateOnBackground(String originalText) {
+
+//        String u = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150616T094415Z.ab702c0fb8064c33.a4bb58f089d2c9167ce05c0078ffd982a25f982e&lang=en-ru&text=To+be,+or+not+to+be%3F&text=That+is+the+question.'
+        String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=${API_KEY}&lang=en-ru&text=${originalText}"
+        Request request = new Request.Builder()
+                .url(url)
+                .build()
+        Response response = client.newCall(request).execute()
+        String jsonText = response.body().string()
+
+        JSONObject jsonObj = new JSONObject(jsonText);
+
+        def code = jsonObj.getInt('code')
+        JSONArray text = jsonObj.getJSONArray('text')
+
+
+        def string = text.join()
+        doOnUIThread(code == 200 ? string : "Server unavailable")
+    }
+
+    @OnUIThread()
+    public void doOnUIThread(String translatedText) {
+        txvTranslate.visibility = View.VISIBLE
+        txvTranslate.setText(translatedText)
+
+
+        prbTranslate.setIndeterminate false
+
+    }
+
+
+
+
+}
