dc11c0c972c258ee5ae4fd0e66b93e5b0aa36a6a
Serhiy Kyrylyuk <skyryl@softserveinc.com>
2015-11-11 21:59:21 +0200

    Start migration from Groovy to Kotlin/RxAndroid

---
+++ app/src/main/java/com/skyrylyuk/lexicongain/DBModule.kt
@@ -0,0 +1,12 @@
+package com.skyrylyuk.lexicongain
+
+import io.realm.annotations.RealmModule
+
+/**
+ * Created by skyrylyuk on 11/11/15.
+ */
+
+@RealmModule(allClasses = true)
+public open class DBModule {
+
+}
