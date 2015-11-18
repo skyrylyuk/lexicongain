743af48e3fbc4387983560caadc8c30b36b09ca3
Serhiy Kyrylyuk <skyryl@softserveinc.com>
2015-11-18 23:25:47 +0200

    Changed default language translation  on ukraine

--- app/src/main/java/com/skyrylyuk/lexicongain/YandexTranslate.java
+++ app/src/main/java/com/skyrylyuk/lexicongain/YandexTranslate.java
@@ -1,7 +1,6 @@
 package com.skyrylyuk.lexicongain;
 
 import com.google.gson.JsonObject;
-
 import retrofit.http.GET;
 import retrofit.http.Query;
 import rx.Observable;
@@ -13,7 +12,7 @@ import rx.Observable;
 public interface YandexTranslate {
     String HOST = "https://translate.yandex.net";
     String API_KEY = "trnsl.1.1.20150616T094415Z.ab702c0fb8064c33.a4bb58f089d2c9167ce05c0078ffd982a25f982e";
-    String LANG = "en-ru";
+    String LANG = "en-uk";
     String TEXT = "text";
 
     @GET("/api/v1.5/tr.json/translate")
