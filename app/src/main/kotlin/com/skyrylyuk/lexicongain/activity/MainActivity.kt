14a65ab2605e06bbde90fc752e8b2acdef0edc1a
Serhiy Kyrylyuk <skyryl@softserveinc.com>
2016-10-31 01:36:22 +0200

    Implement Add dialog

--- app/src/main/kotlin/com/skyrylyuk/lexicongain/activity/MainActivity.kt
+++ app/src/main/kotlin/com/skyrylyuk/lexicongain/activity/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.*
 import android.view.MotionEvent.*
+import com.afollestad.materialdialogs.MaterialDialog
+import com.github.ajalt.timberkt.Timber.d
 import com.github.ajalt.timberkt.i
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -23,16 +27,16 @@ import com.transitionseverywhere.TransitionManager
 import com.transitionseverywhere.TransitionSet
 import com.transitionseverywhere.extra.Scale
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_add_card.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
 import javax.inject.Inject
 
 /**
  *
  * Created by skyrylyuk on 11/11/15.
  */
-class MainActivity : AppCompatActivity(), AnkoLogger {
+class MainActivity : AppCompatActivity(), AnkoLogger, TextWatcher {
 
     val SENSITIVE: Int = 10
     val THRESHOLD = 15
@@ -63,8 +67,21 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         }
 
         addButton.setOnClickListener {
-            toast("FFAB")
-            txvTranslateText.visibility
+            val dialog = MaterialDialog.Builder(this)
+                    .title(R.string.add_card_title)
+                    .customView(R.layout.dialog_add_card, false)
+                    .positiveText(R.string.add_card_agree)
+                    .onPositive { dialog, which ->
+                        val newOriginal = dialog.view.newOriginalText.text
+                        val newTranslate = dialog.view.newTranslateText.text
+                        i { "==> $newOriginal $newTranslate" }
+                    }
+                    .negativeText(R.string.add_card_disagree)
+                    .show()
+
+
+            dialog.view.newOriginalText.addTextChangedListener(this)
+
         }
 
 
@@ -151,6 +168,18 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        d { "==> p0 = ${p0}" }
     }
 
     override fun onDestroy() {
